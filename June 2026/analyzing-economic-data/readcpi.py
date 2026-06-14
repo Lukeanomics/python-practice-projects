24a2eb80a38a14ed923e6095980b687a0770ab14
LukeM <lmikho@carthage.edu>
2026-06-13 19:21:00 -0500

    reading cpi data from csv file and loading it into a pandas dataframe

--- June 2026/analyzing-economic-data/readcpi.py
+++ June 2026/analyzing-economic-data/readcpi.py
@@ -1,1 +1,20 @@
+import pandas as pd
 
+# Read CPI Data From CSV File
+
+df = pd.read_csv('cpi-release.csv')
+df.head()
+
+# Filter Out Data
+df[df['Indent-Level'] < 3]
+
+df2 = df.filter(items=['Indent-Level', 
+                       'Expenditure-category', 
+                       'SU-index-Apr.2026', 
+                       'SU-index-May2026', 
+                       'SU%-Apr.2026-May2026']
+                )
+
+df = df2[df2['Indent-Level'] < 3]
+
+print(df)
